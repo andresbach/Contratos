ec11c51321b67a2a51e9458e6d914353c7161e3b
Andres Bachfischer <andresbach@gmail.com>
2018-09-20 11:00:00 -0300

    Modified Tictactoe contract & its compilation

---
+++ TicTacToe2.sol
@@ -0,0 +1,228 @@
+// solc TicTacToe.sol --bin --abi --optimize --overwrite -o Compilado/
+
+pragma solidity ^0.4.23;
+
+contract TicTacToe2 {
+    //uint constant public gameCost = 0.1 ether;
+		uint constant public gameCost = 10;
+
+    uint8 public boardSize = 3;
+    uint8 movesCounter;
+
+    bool public gameActive;
+
+    address[3][3] board;
+
+    address public player1;
+    address public player2;
+
+    uint balanceToWithdrawPlayer1;
+    uint balanceToWithdrawPlayer2;
+
+    uint timeToReact = 30 minutes;
+    uint gameValidUntil;
+		uint activeParticipants;
+
+    address activePlayer;
+
+    event PlayerJoined(address player);
+    event NextPlayer(address player);
+    event GameOverWithWin(address winner);
+    event GameOverWithDraw();
+    event PayoutSuccess(address receiver, uint amountInWei);
+
+    function joinGame() public payable {
+        assert(!(player1 != address(0) && player2 != address(0)));
+				assert(msg.sender != player1);
+        gameActive = true;
+
+        require(msg.value == gameCost);
+
+				if(activeParticipants == 0){
+					player1 = msg.sender;
+					activeParticipants++;
+					delete board;
+				} else {
+					player2 = msg.sender;
+					activeParticipants++;
+				}
+
+        emit PlayerJoined(msg.sender);
+
+				if(activeParticipants == 2) {
+					if(block.number % 2 == 0) {
+	            activePlayer = player2;
+	        } else {
+	            activePlayer = player1;
+	        }
+
+	        gameValidUntil = now + timeToReact;
+
+	        emit NextPlayer(activePlayer);
+				}
+
+    }
+
+    function getBoard() public view returns(address[3][3]) {
+        return board;
+    }
+
+    function setWinner(address player) private {
+        gameActive = false;
+        //emit an event
+        emit GameOverWithWin(player);
+        uint balanceToPayOut = address(this).balance;
+        if(player.send(balanceToPayOut) != true) {
+            if(player == player1) {
+                balanceToWithdrawPlayer1 = balanceToPayOut;
+            } else {
+                balanceToWithdrawPlayer2 = balanceToPayOut;
+            }
+        } else {
+            emit PayoutSuccess(player, balanceToPayOut);
+						activeParticipants = 0;
+						player1 = address(0);
+						player2 = address(0);
+        }
+        //transfer money to the winner
+
+    }
+
+    function withdrawWin() public {
+      uint balanceToTransfer;
+        if(msg.sender == player1) {
+            require(balanceToWithdrawPlayer1 > 0);
+            balanceToTransfer = balanceToWithdrawPlayer1;
+            balanceToWithdrawPlayer1 = 0;
+            player1.transfer(balanceToTransfer);
+
+            emit PayoutSuccess(player1, balanceToTransfer);
+
+						activeParticipants = 0;
+						player1 = address(0);
+						player2 = address(0);
+        } else {
+
+            require(balanceToWithdrawPlayer2 > 0);
+            balanceToTransfer = balanceToWithdrawPlayer2;
+            balanceToWithdrawPlayer2 = 0;
+            player2.transfer(balanceToTransfer);
+            emit PayoutSuccess(player2, balanceToTransfer);
+
+						activeParticipants = 0;
+						player1 = address(0);
+						player2 = address(0);
+        }
+    }
+
+    function setDraw() private {
+        gameActive = false;
+        emit GameOverWithDraw();
+
+        uint balanceToPayOut = address(this).balance/2;
+
+        if(player1.send(balanceToPayOut) == false) {
+            balanceToWithdrawPlayer1 += balanceToPayOut;
+        } else {
+            emit PayoutSuccess(player1, balanceToPayOut);
+        }
+        if(player2.send(balanceToPayOut) == false) {
+            balanceToWithdrawPlayer2 += balanceToPayOut;
+        } else {
+            emit PayoutSuccess(player2, balanceToPayOut);
+        }
+
+				activeParticipants = 0;
+				player1 = address(0);
+				player2 = address(0);
+
+    }
+
+    function emergecyCashout() public {
+        require(gameValidUntil < now);
+        require(gameActive);
+        setDraw();
+    }
+
+
+    function setStone(uint8 x, uint8 y) public {
+        require(board[x][y] == address(0));
+        require(gameValidUntil > now);
+        assert(gameActive);
+        assert(x < boardSize);
+        assert(y < boardSize);
+        require(msg.sender == activePlayer);
+        board[x][y] = msg.sender;
+        movesCounter++;
+        gameValidUntil = now + timeToReact;
+
+        for(uint8 i = 0; i < boardSize; i++) {
+            if(board[i][y] != activePlayer) {
+                break;
+            }
+            //win
+            if(i == boardSize - 1) {
+                //winner
+                setWinner(activePlayer);
+                return;
+            }
+        }
+        for(i = 0; i < boardSize; i++) {
+            if(board[x][i] != activePlayer) {
+                break;
+            }
+            //win
+
+            if(i == boardSize - 1) {
+                //winner
+                setWinner(activePlayer);
+                return;
+            }
+        }
+
+        //diagonale
+        if(x == y) {
+            for(i = 0; i < boardSize; i++) {
+                if(board[i][i] != activePlayer) {
+                    break;
+                }
+                //win
+                if(i == boardSize - 1) {
+                    //winner
+                    setWinner(activePlayer);
+                    return;
+                }
+            }
+        }
+
+        //anti-diagonale
+        if((x+y) == boardSize-1) {
+            for(i = 0; i < boardSize; i++) {
+                if(board[i][(boardSize-1)-i] != activePlayer) {
+                    break;
+                }
+                //win
+
+                if(i == boardSize - 1) {
+                    //winner
+                    setWinner(activePlayer);
+                    return;
+                }
+            }
+        }
+
+        //draw
+        if(movesCounter == (boardSize**2)) {
+            //draw
+            setDraw();
+            return;
+        }
+
+        if(activePlayer == player1) {
+            activePlayer = player2;
+        } else {
+            activePlayer = player1;
+        }
+        emit NextPlayer(activePlayer);
+    }
+}
